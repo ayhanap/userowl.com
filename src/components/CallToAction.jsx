f2d8f7f6d699d75d65305421629349849c79e8d0
Ayhan APAYDIN <ayhanap@gmail.com>
2022-09-13 14:08:33 +0300

    fixed &apos; in CallToAction.jsx

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -14,7 +14,7 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            <span className="block">Know when it's ready.</span>
+            <span className="block">Know when it&apos;s ready.</span>
             <span className="block">Get special early access discount.</span>
           </h2>
           <p className="mt-4 text-lg leading-6 text-indigo-200">
