04f70b6962985a9e10e1aaac7da530d863dbc336
Ayhan APAYDIN <ayhanap@gmail.com>
2022-09-20 02:42:28 +0300

    applied color palette

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,12 @@ module.exports = {
     'postcss-focus-visible': {
       replaceWith: '[data-focus-visible-added]',
     },
+    'postcss-lab-function': {
+      preserve: true,
+      subFeatures: {
+        displayP3: false
+      }
+    },
     autoprefixer: {},
   },
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,9 +84,10 @@ export function Hero() {
                     id="email"
                     className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
                     placeholder="Enter your email"
+                    required
                   />
                   <button
-                    className="ml-onclick-form mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
+                    className="ml-onclick-form mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
                   >
                     Notify me
                   </button>
