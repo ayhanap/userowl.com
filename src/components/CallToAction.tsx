36e3fcf1c665fa41aa3d3e23a128b7c9a100bfe4
Ayhan APAYDIN <ayhanap@gmail.com>
2023-09-06 12:40:43 +0300

    fix eslint errors

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-import { Button } from '@/components/Button'
-import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-call-to-action.jpg'
+import { Button } from '@/components/Button';
+import { Container } from '@/components/Container';
+import backgroundImage from '@/images/background-call-to-action.jpg';
 
+// eslint-disable-next-line import/prefer-default-export
 export function CallToAction() {
   return (
-    <section
-      id="get-started-today"
-      className="relative overflow-hidden bg-blue-600 py-32"
-    >
+    <section id="get-started-today" className="relative overflow-hidden bg-blue-600 py-32">
       <Image
         className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
         src={backgroundImage}
@@ -24,8 +22,8 @@ export function CallToAction() {
             Get started today
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+            It’s time to take control of your books. Buy our software so you can feel like you’re
+            doing something productive.
           </p>
           <Button href="/register" color="white" className="mt-10">
             Get 6 months free
@@ -33,5 +31,5 @@ export function CallToAction() {
         </div>
       </Container>
     </section>
-  )
+  );
 }
