e37d750adf28e308c4f24d5d4da862f9c3f7a4b0
Ayhan APAYDIN <ayhanap@gmail.com>
2023-10-24 16:37:40 +0300

    added userowl widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from '@/components/Footer';
 import { Header } from '@/components/Header';
 import '@/styles/tailwind.css';
 import { type Metadata } from 'next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: {
@@ -45,6 +46,41 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={clsx('h-full scroll-smooth bg-white antialiased', inter.variable, lexend.variable)}
       suppressHydrationWarning
     >
+      <head>
+        <Script id="userowl-widget" strategy="afterInteractive">
+          {`
+          //Your APP ID
+          var APP_ID = '6439840082e47f1eafd39804';
+
+          window.UserowlSettings = {
+            appId: APP_ID
+          };
+
+          (function() {
+            var w = window;
+            var uo = w.Userowl = w.Userowl || [];
+            uo.methods = ["open", "close"];
+            w.UserowlQueue = [];
+            uo.f = function(c) {
+              return function() {
+                var args = Array.prototype.slice.call(arguments);
+                window.UserowlQueue.push({
+                  c: c,
+                  a: args
+                })
+              }
+            }
+            for(i = 0; i < uo.methods.length; i++){
+              uo[uo.methods[i]] = uo.f(uo.methods[i]);
+            }
+            var d = document;
+            var s = d.createElement('script');
+            s.async = true;
+            s.src = 'https://app.userowl.com/static/widget.js';
+            (d.head || d.body).appendChild(s);
+          })();`}
+        </Script>
+      </head>
       <body className="">
         <Providers>
           {/* <div className="flex w-full">
