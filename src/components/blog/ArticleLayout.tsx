dfbf861e1d77f056be08d8e39ce03b146f84929c
Ayhan APAYDIN <ayhanap@gmail.com>
2023-11-14 01:24:22 +0300

    improve some blog layout

--- src/components/blog/ArticleLayout.tsx
+++ src/components/blog/ArticleLayout.tsx
@@ -36,13 +36,13 @@ export default function ArticleLayout({
   const { previousPathname } = useContext(AppContext);
 
   return (
-    <Container className="mt-16 lg:mt-32">
+    <Container className="mt-16 lg:mt-24">
       <div className="xl:relative">
-        <div className="mx-auto max-w-2xl">
+        <div className="mx-auto max-w-3xl">
           <Link
             href="/blog"
             aria-label="Go back to articles"
-            className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-gray-800/5 ring-1 ring-gray-900/5 transition dark:border dark:border-gray-700/50 dark:bg-gray-800 dark:ring-0 dark:ring-white/10 dark:hover:border-gray-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
+            className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-gray-800/5 ring-1 ring-gray-900/5 transition dark:border dark:border-gray-700/50 dark:bg-gray-800 dark:ring-0 dark:ring-white/10 dark:hover:border-gray-700 dark:hover:ring-white/20 lg:absolute lg:left-8 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
           >
             <ArrowLeftIcon className="h-4 w-4 stroke-gray-500 transition group-hover:stroke-gray-700 dark:stroke-gray-500 dark:group-hover:stroke-gray-400" />
           </Link>
