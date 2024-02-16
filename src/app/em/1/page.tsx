5851cc15f26cf12adb60d1812cc6e03a0ee66860
Ayhan APAYDIN <ayhanap@gmail.com>
2024-02-16 15:42:09 +0300

    added employee page

---
+++ src/app/em/1/page.tsx
@@ -0,0 +1,11 @@
+import { type Metadata } from 'next';
+
+export default async function EmployeeAyhanApaydin() {
+  return <div>Ayhan APAYDIN</div>;
+}
+
+export const metadata: Metadata = {
+  title: 'Ayhan APAYDIN',
+  // description:
+  //   'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+};
